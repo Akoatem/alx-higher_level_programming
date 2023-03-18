21ed8372f8cfe43af72241983931a474edef02e6
Akoatem <akoatemsam2003@gmail.com>
2023-03-18 07:21:16 +0200

    create database and table

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    mydb = MySQLdb.connect(
+        host="localhost", 
+        user=argv[1], 
+        port=3306, 
+        passwd=argv[2], 
+        db=argv[3]
+        )
+    
+    mycursor = mydb.cursor()
+    
+    mycursor.execute("SELECT * FROM states")
+
+    rows = mycursor.fetchall()
+
+    for row in rows:
+        print(row)
