1d29b1399ae73f18158ba84fdbc37d3ee9129f99
Akoatem <akoatemsam2003@gmail.com>
2023-03-18 09:56:33 +0200

    Task1 updated

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,12 +13,14 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    mydb = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM states \
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
                  WHERE name LIKE BINARY 'N%' \
                  ORDER BY states.id ASC")
-    rows = mycursor.fetchall()
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
