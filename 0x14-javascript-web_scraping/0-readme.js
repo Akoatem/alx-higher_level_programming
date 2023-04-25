e7a2b687b96e6dc042ecea4e270c685923568a1e
Akoatem <akoatemsam2003@gmail.com>
2023-04-25 13:46:33 +0200

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-/** https://www.youtube.com/watch?v=zMX3gqs3Y7I**/
 
 const fs = require('fs');
-//const filename = process.argv[2];
-//using argv[] shows that it will pass whatever parameter of filename
+const filename = process.argv[2];
 
-fs.readFile('text.txt', 'utf8', (err, data) =>{
-	if(err){
-		console.log(err);
-	}else{
-		console.log(data);
-	}
+fs.readFile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
 });
